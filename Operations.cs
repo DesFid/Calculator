9a5b991f049410000313e2fe57dd6c5d3cc4210b
DesFid <35884517+DesFid@users.noreply.github.com>
2018-06-03 19:44:29 -0500

    Add files via upload

---
+++ Operations.cs
@@ -0,0 +1,20 @@
+using System;
+public class Operations{
+    public float Addition(float num1, float num2)
+    {
+        return num1 + num2;
+    }
+    public float Substraction(float num1, float num2)
+    {
+        return num1 - num2;
+    }
+    public float Multiplication(float num1, float num2)
+    {
+        return num1 * num2;
+    }
+    public float Division(float num1, float num2)
+    {
+        return num1 / num2;
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculator {
+    class Program {
+        static void Main (string[] args) {
+            float total = 0;
+            Operations oper = new Operations ();
+            Console.WriteLine ("Calculator\n\tOptions\nAddition\tA\nSubstracion\tB\nMultiplication\tC\nDivision\tD\nTotal\t\tE\n");
+            Console.Write ("Insert a value: ");
+            string input = Console.ReadLine ();
+            Console.Write ("Choose a option: ");
+            string operation = Console.ReadLine ();
+            if (float.TryParse (input, out float number1)) {
+                while (operation != "E") {
+                    Console.Write ("Insert other value: ");
+                    string input_2 = Console.ReadLine ();
+                    if (float.TryParse (input_2, out float number2)) {
+                        if (operation == "A") {
+                            total = oper.Addition (number1, number2);
+                            number1 = total;
+                        } else if (operation == "B") {
+                            total = oper.Substraction (number1, number2);
+                            number1 = total;
+                        } else if (operation == "C") {
+                            total = oper.Multiplication (number1, number2);
+                            number1 = total;
+                        } else if (operation == "D") {
+                            total = oper.Division (number1, number2);
+                            number1 = total;
+                        }
+                    } else {
+                        Console.WriteLine ("ERROR: You can't insert a string");
+                        Environment.Exit(0);
+                    }
+                    Console.Write ("Choose a option: ");
+                    operation = Console.ReadLine ();
+                    
+                }
+                Console.WriteLine ("Total: " + number1);
+            } else {
+                Console.WriteLine ("ERROR: You can't insert a string");
+            }
+        }
+    }
+}
